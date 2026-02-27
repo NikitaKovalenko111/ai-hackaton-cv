4f3d7e4f6d0b0de002fcebbae0d3a06c9ad64909
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2026-02-27 19:56:02 +0500

    added params block

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
 import { useCallback, useState, type ReactEventHandler } from 'react';
+import cn from 'classnames';
 import './style.css'
 import { useDropzone } from 'react-dropzone';
 
 function App() {
   const [file, setFile] = useState<string | null>(null)
+  const [isVisible, setIsVisible] = useState<boolean>(false)
 
   const onDrop = useCallback((acceptedFiles: any) => {
     const url = URL.createObjectURL(acceptedFiles[0])
 
     setFile(url)
   }, []);
+
+  const sendHandler = () => {
+    setIsVisible(true)
+  }
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
@@ -37,8 +43,29 @@ function App() {
               <p>Поместите сюда изображение или нажмите, чтобы выбрать</p>
             }
           </div>
+          <button className='main__send-button' onClick={sendHandler}>Отправить</button>
         </div>
-        <button className='main__send-button'>Отправить</button>
+        <div className={cn('main_wrapper', 'main__params', {'main__params--visible': isVisible})}>
+          <h2 className="main_header">Параметры</h2>
+          <div className="main__labels">
+            <div className="main__label">
+              <h3>Тип растения:</h3>
+              <span>Пшеница</span>
+            </div>
+            <div className="main__label">
+              <h3>Длина корня:</h3>
+              <span>5 см</span>
+            </div>
+            <div className="main__label">
+              <h3>Длина стебля:</h3>
+              <span>6 см</span>
+            </div>
+            <div className="main__label">
+              <h3>Площадь листиков:</h3>
+              <span>12 см^2</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
