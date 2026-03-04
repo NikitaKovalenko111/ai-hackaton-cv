1eda90b7a524c14bdf38397f2106c6476d460019
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2026-03-04 20:06:33 +0500

    added inference

--- model/check.py
+++
@@ -1,3 +0,0 @@
-from classificator import classificator
-
-classificator.create_dataset_csv('model/dataset', 'dataset_measurements.csv', 85, 'model/runs/segment/plant_seg_v12/weights/best.pt')
---
+++ model/inference.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+from utils import model_math
+
+print("Введите путь до модели: ")
+
+model_path = input()
+
+print("Введите путь до изображения: ")
+
+img_path = input()
+
+model = YOLO(model_path)
+
+results = model.predict(img_path, save=True, conf=0.1)
+
+measures = model_math.measure_objects(results)
+
+for m in measures[0]:
+    print(f"Класс: {m['class']}")
+    print(f"Длина в пикселях: {m['length_px']}")
+    print(f"Длина в сантиметрах: {m['length_px'] / model_math.getPpc()}")
+    print(f"Площадь в пикселях: {m['area_px']}")
+    print(f"Площадь в сантиметрах: {m['area_px'] / (model_math.getPpc() ** 2)}")
